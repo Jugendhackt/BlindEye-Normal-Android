836a90cd93f4e7c5cf3f69efc584706e57882897
HyTeYT <mail@marcoalexanderfischer.de>
2018-11-24 14:58:08 +0100

    :lipstick: Added logo

--- app/src/main/java/de/marcoalexanderfischer/blindeye/blindeyeassist/MainActivity.kt
+++ app/src/main/java/de/marcoalexanderfischer/blindeye/blindeyeassist/MainActivity.kt
@@ -1,11 +1,15 @@
 package de.marcoalexanderfischer.blindeye.blindeyeassist
 
 import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private var locationManager : LocationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,17 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent);
         }
 
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?;
+        locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            location.longitude
+            location.latitude
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 }
